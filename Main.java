b96f0cee16880f5b22585b6792bb4c1f98c06dd0
Ashley Renee Smith <ashleyreneesmith@zipcoders-MacBook-Pro-2.local>
2019-02-05 18:36:19 -0500

    changes to the guessing game

--- Main.java
+++ Main.java
@@ -5,29 +5,73 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+
+import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to the Guessing game");
+        System.out.println("Pick a number: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int maxNum;
+        int numOfGuesses = 0;
+        int results = 0;
+        int guess;
+        boolean playAgain;
+        
+        do {
+        maxNum = random.nextInt(50);
+        guess = 0;
+        playAgain = false;
+        
+        while(guess != maxNum) {
+            System.out.println("Please enter a number 1 through 50");
+            guess = scanner.nextInt();
+            numOfGuesses++;
+            
+            if (guess < 1 || guess > 50) {
+               System.out.println("Invaild Input"); 
+        
+            }else if(guess < maxNum) {
+                System.out.println("Your guess is too low");
+              
+            } else if(guess > maxNum) {
+                System.out.println("Your guess is too high");
+            }
+        }
+        results++;
+        System.out.println("Great Job you won! Your number of guesses was: " + numOfGuesses + " and the number was: " + maxNum);
+        System.out.println("Would you like to play again?");
+        
+        switch(scanner.next()) {
+            
+           case "yes":
+            playAgain = true;
+                break;
+           default:
+                break;
+            }
+        } while(playAgain);
+        System.out.println("Thank you Goodbye!");
 }
+}
+
+
+
+        
+        
+       
+        
+      
+        
+        
+        
+        
+        
+        
+    
+
